62798e50d176e7389f2db5a96b56b6e31d7d698c
PanayiotaK <panagiota.konstantinou@durham.ac.uk>
2019-03-28 08:49:27 +0000

    get requests

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-var express = require('express')
-var app = express()
-app.get('/',function(req,resp){
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var obj1 = JSON.parse(fs.readFileSync("dogs.json"));
+var obj2 = JSON.parse(fs.readFileSync("volunteers.json"));
+app.use(express.static('client'));
+app.get('/dogs',function(req,resp){    
+    resp.send(obj1);
 
 
+});
 
-})
+app.get('/volunteers',function(req,resp){
+    resp.send(obj2);
+
+});
+
+app.get('/owners',function(req,resp){
+    resp.send(obj1);
+});
 
 app.listen (8090)
